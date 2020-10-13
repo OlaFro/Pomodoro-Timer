525bd7520cbf8156f03bc2b1d6e7227973a72847
Ola Frost <Ola@MacBook-Air-Aleksandra.local>
2020-10-13 20:03:47 +0200

    filling the dots works

--- script.js
+++ script.js
@@ -10,22 +10,25 @@ const displayB = document.getElementById("displayB");
 const info = document.getElementById("info");
 const circles = document.querySelectorAll("box")
 
+
 // app variables
 let minutesInSession = 25;
 let minutesInBreak = 3;
 let secondsInTotal;
 var counting;
 let timeForBreak = false;
-let cycle = 0;
+let sessionCounter = 1;
+let longBreak = false;
+
 
 // adjusting the duration of session/break
 minS.addEventListener("input", () => {
   displayS.textContent = "Session " + minS.value + " min";
   minutesInSession = parseInt(minS.value);
-  secondsInTotal = minutesInSession * 60;
-  minutes.textContent = parseInt(minS.value);
+  minutes.textContent = minutesInSession;
   return minutesInSession;
 });
+
 minB.addEventListener("input", () => {
   displayB.textContent = "Break " + minB.value + " min";
   minutesInBreak = parseInt(minB.value);
@@ -40,10 +43,28 @@ startBtn.addEventListener("click", () => {
 });
 
 function sessionMode() {
-  console.log("session");
   // two lines to help the counter start right away:
   // secondsInTotal--;
   // countDown();
+
+  switch(sessionCounter){
+    case 1:
+      document.getElementById("circle1").style.backgroundColor="white"
+      break
+    case 2:
+      document.getElementById("circle2").style.backgroundColor="white"
+    break
+    case 3:
+      document.getElementById("circle3").style.backgroundColor="white"
+    break
+    case 4:
+      document.getElementById("circle4").style.backgroundColor="white"
+      timeForBreak = false;
+      longBreak = true;
+    break
+    default:
+  }
+  
 
   info.textContent = "You are in the session";
   document.body.style.color = "white";
@@ -56,7 +77,6 @@ function breakMode() {
   console.log("break");
   minutesInBreak = minB.value;
   minutes.textContent = minutesInBreak;
-  seconds.textContent = "00";
   secondsInTotal = minutesInBreak * 60;
   info.textContent = `Break for ${minB.value} minutes!`;
   document.body.style.color = "black";
@@ -73,18 +93,23 @@ function countDown() {
   }
 
   if (secondsInTotal === 0 && timeForBreak == true) {
-
+    sessionCounter++
     clearInterval(counting);
     breakMode();
   }
 
-  if (secondsInTotal === 0 && timeForBreak == false) {
+  if (secondsInTotal === 0 ) {
     clearInterval(counting);
     sessionMode();
   }
-  //   console.log(secondsInTotal);
-  //   console.log(new Date().getSeconds());
+ 
+  if (secondsInTotal === 0 && sessionCounter===4){
+    clearInterval(counting)
+    
+  }
+
   secondsInTotal--;
+
   console.log(secondsInTotal);
 }
 
@@ -93,10 +118,16 @@ stopBtn.addEventListener("click", () => {
   startBtn.classList.toggle("hidden");
   stopBtn.classList.toggle("hidden");
   clearInterval(counting);
+  sessionCounter = 1
+  document.getElementById("circle1").style.backgroundColor= ""
+  document.getElementById("circle2").style.backgroundColor= ""
+  document.getElementById("circle3").style.backgroundColor= ""
+  document.getElementById("circle4").style.backgroundColor= ""
+
   minutes.textContent = minS.value;
   seconds.textContent = "00";
   secondsInTotal = minutesInSession * 60;
-  info.textContent = "Press start";
+  info.textContent = "Press start"; 
 });
 
 
@@ -105,11 +136,12 @@ stopBtn.addEventListener("click", () => {
 
 /*
 BUGS:
-none at this stage
+
 
 PROBLEMS:
 
-filling each dot after each session
-
+IN CASE OF UNEVEN SECONDS:
+console.log(secondsInTotal);
+console.log(new Date().getSeconds());
 
 */
